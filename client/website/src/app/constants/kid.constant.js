a27da017a3b3506cb1bc6e9b6a94c13bc9f0975c
trickyboy67 <brianjunk@hotmail.fr>
2015-04-02 23:00:47 +0200

    Toast helper try

---
+++ client/website/src/app/constants/kid.constant.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('kid')
+  .constant('kidConstant', {
+    toastPosition : {bottom: false, top: true, left: false, right: true},
+
+  });
---
+++ client/website/src/app/helpers/toast.helper.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('kid')
+  .service('toastHelper', {
+    toastPosition : {bottom: false, top: true, left: false, right: true},
+
+  });
