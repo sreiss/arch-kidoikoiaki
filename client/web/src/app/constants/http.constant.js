518ffec9c288e630bc94984c4b9ed35e68d483a1
Brian <brianjunk@hotmail.fr>
2015-05-29 00:58:50 +0200

    Update http.constant.js

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -2,7 +2,7 @@
 
 angular.module('kid').constant('httpConstant',
 {
-  //apiUrl: 'http://localhost:3024'
-  apiUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3024'
+  apiUrl: 'http://localhost:3024'
+  //apiUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3024'
   
 });
