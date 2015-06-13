b99c353b341a2fa9a57f21af1318f2b545a43554
Maxime RAUCH <maxime@rauch.fr>
2015-06-13 13:57:12 +0200

    Fix charts data.

--- client/web/src/components/transaction/arch-transaction.service.js
+++ client/web/src/components/transaction/arch-transaction.service.js
@@ -142,7 +142,7 @@ angular.module('kid')
           data = JSON.stringify(data);
           console.log(data);
           data = data.replace(',"', ',');
-          data = data.replace('"]', '');
+          data = data.replace('"]', ']');
           console.log(data);
 
           var chart = {
